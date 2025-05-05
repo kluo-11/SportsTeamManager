5028800a6c77852e500d709b08efd0fd212ff39c
kluo11 <kluo11@icloud.com>
2025-05-05 00:38:31 -0400

    final commit before demo

---
+++ mySQL_files/test_queries.sql
@@ -0,0 +1,14 @@
+-- create database sports_team_manager;
+
+select * from users;
+select * from teams;
+
+INSERT INTO Users (username, password_hash, email)
+VALUES ('admin', 'testhash', 'admin@example.com');
+
+select * from attendance;
+select * from users;
+select * from teams;
+select * from players;
+select * from events;
+select * from matches;
