8889cd60c140dda50b4fcd0312f20a58dc5fa070
kluo11 <kluo11@icloud.com>
2025-05-04 22:09:18 -0400

    updated indexes file

--- mySQL_files/create_indexes.sql
+++ mySQL_files/create_indexes.sql
@@ -3,3 +3,4 @@ CREATE INDEX idx_players_team_id ON Players(team_id);
 CREATE INDEX idx_events_team_id ON Events(team_id);
 CREATE INDEX idx_events_event_date ON Events(event_date);
 CREATE INDEX idx_events_team_date ON Events(team_id, event_date);
+CREATE INDEX idx_matches_event_id ON Matches(event_id);
